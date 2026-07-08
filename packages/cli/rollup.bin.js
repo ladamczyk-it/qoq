b793eb1ef60aac95bf8ce5f3be34c998deee26ac
Lukasz Adamczyk <ladamczyk.it@gmail.com>
2026-07-08 10:01:56 +0200

    chore: cleanup

--- packages/cli/rollup.bin.js
+++ packages/cli/rollup.bin.js
@@ -27,6 +27,7 @@ const external = [
   ...builtinModules,
   ...Object.keys(pkg.dependencies),
   ...Object.keys(pkg.peerDependencies),
+  'structurelint',
   'stylelint',
   'jscpd',
   'prettier',
